70d56df853b53e868eacfbe1ee272c4c4ab7c7a3
Nazar <wolfter12@gmail.com>
2021-03-14 20:31:43 +0200

    Added UploadFile component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import UploadFile from '../UploadFile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <UploadFile />
     </div>
   );
 }
