fbed90ef34b97992dd53a08bc6ff9f6f571cf62a
Nazar <wolfter12@gmail.com>
2021-03-15 23:00:51 +0200

    Added phone validation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,3 +1,5 @@
+import { PHONE } from '../configs/header-accessors';
+
 export const isFullNameValid = (value) => {
   if (typeof value !== 'string' || value.length === 0) {
     return false;
@@ -5,8 +7,21 @@ export const isFullNameValid = (value) => {
   return true;
 };
 
+export const isPhoneValid = (value) => {
+  if (typeof value !== 'string' || value.length === 0) {
+    return false;
+  }
+  const isValid =
+    /^\+1\d{10}$/.test(value) ||
+    /^1\d{10}$/.test(value) ||
+    /^\d{10}$/.test(value);
+  return isValid;
+};
+
 const validator = (value, header) => {
   switch (header) {
+    case PHONE:
+      return isPhoneValid(value);
     default:
       return true;
   }
