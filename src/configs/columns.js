02aaefcbfaa2b6100e772320b3e98d9c9e028466
Nazar <wolfter12@gmail.com>
2021-03-15 14:44:32 +0200

    Added columns config Header - accessor

---
+++ src/configs/columns.js
@@ -0,0 +1,52 @@
+const columns = [
+  {
+    Header: 'ID',
+    accessor: 'id',
+  },
+  {
+    Header: 'Full Name',
+    accessor: 'fullName',
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone',
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+  },
+  {
+    Header: 'Experience',
+    accessor: 'experience',
+  },
+  {
+    Header: 'Yearly Income',
+    accessor: 'yearlyIncome',
+  },
+  {
+    Header: 'Has children',
+    accessor: 'hasChildren',
+  },
+  {
+    Header: 'License states',
+    accessor: 'licenseStates',
+  },
+  {
+    Header: 'Expiration date',
+    accessor: 'expirationDate',
+  },
+  {
+    Header: 'License number',
+    accessor: 'licenseNumber',
+  },
+  {
+    Header: 'Duplication',
+    accessor: 'duplication',
+  },
+];
+
+export default columns;
