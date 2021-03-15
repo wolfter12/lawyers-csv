e5b4c3dffd37ae6e068914e9d98697f715e531e6
Nazar <wolfter12@gmail.com>
2021-03-15 21:11:01 +0200

    Added id and duplication fields

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,13 +1,17 @@
 import { PARSE_FILE } from './types';
 import csv from '../parser/csv';
+import getDuplication from '../utils/mark-duplication';
 
 export const parseFile = (file) => (dispatch) => {
   if (file instanceof Blob) {
     csv(file)
-      .then((results) => {
+      .then(({ data }) => data.map((obj, idx) => ({ id: idx + 1, ...obj })))
+      .then((data) => getDuplication(data))
+      .then((data) => {
+        console.log(data);
         dispatch({
           type: PARSE_FILE,
-          payload: results.data,
+          payload: data,
         });
       })
       .catch((err) => {
