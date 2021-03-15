38e8301a777b6810ec76b662aef43afc80fdfe17
Nazar <wolfter12@gmail.com>
2021-03-15 14:45:55 +0200

    Added Table component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UploadFile from '../UploadFile';
+import Table from '../Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Hello World!</h1>
       <UploadFile />
+      <Table />
     </div>
   );
 }
