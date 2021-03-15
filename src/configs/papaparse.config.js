b719f95fd293fdee891a72843b04de0a5ca0d1e2
Nazar <wolfter12@gmail.com>
2021-03-15 15:49:46 +0200

    Added header and data normalization

--- src/configs/papaparse.config.js
+++ src/configs/papaparse.config.js
@@ -1,10 +1,25 @@
-import camelCase from 'camelcase';
+import _ from 'lodash';
+
+const dataNormalize = (value, header) => {
+  if (typeof value !== 'string') {
+    return value;
+  }
+  let str = _.trim(value, `'" `);
+  if (header === 'email') {
+    str = _.toLower(str);
+  }
+  return str;
+};
+
+const headerNormalize = (value) => _.camelCase(value.toLowerCase());
 
 const options = {
   header: true,
   worker: false, // when true there is a problem with next parsing
   skipEmptyLines: true,
-  transformHeader: camelCase,
+  transformHeader: headerNormalize,
+  comments: true,
+  transform: dataNormalize,
 };
 
 export default options;
