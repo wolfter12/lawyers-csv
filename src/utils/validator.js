31417dadb94c919777a4254a227e0a5b625d73ba
Nazar <wolfter12@gmail.com>
2021-03-16 00:24:43 +0200

    Added yearly income validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,5 +1,12 @@
-import { AGE, EMAIL, EXPERIENCE, PHONE } from '../configs/header-accessors';
+import {
+  AGE,
+  EMAIL,
+  EXPERIENCE,
+  PHONE,
+  YEARLY_INCOME,
+} from '../configs/header-accessors';
 import { validate as emailValidator } from 'email-validator';
+import { MAX_YEARLY_INCOME } from '../configs/constants';
 
 export const isFullNameValid = (value) => {
   if (typeof value !== 'string' || value.length === 0) {
@@ -46,16 +53,31 @@ export const isExperienceValid = (value, age = 21) => {
     return number >= 0 && number <= currentAge - 21;
   };
   const ageType = typeof age;
-  const ageValue = ageType === 'number'
-    ? age
-    : ageType === 'string'
-      ? Number(age)
-      : 0;
+  const ageValue =
+    ageType === 'number' ? age : ageType === 'string' ? Number(age) : 0;
   switch (typeof value) {
     case 'number':
       return checkExperience(value, ageValue);
     case 'string':
       return checkExperience(Number(value), ageValue);
+    default:
+      return false;
+  }
+};
+
+export const isYearlyIncomeValid = (value) => {
+  const checkYearlyIncome = (income) => {
+    return income >= 0 && income <= MAX_YEARLY_INCOME;
+  };
+  switch (typeof value) {
+    case 'number':
+      return checkYearlyIncome(value);
+    case 'string':
+      const yearlyIncomeNumber = Number(value);
+      if (isNaN(yearlyIncomeNumber)) {
+        return false;
+      }
+      return checkYearlyIncome(yearlyIncomeNumber);
     default:
       return false;
   }
@@ -70,8 +92,9 @@ const validator = (value, header, row) => {
     case AGE:
       return isAgeValid(value);
     case EXPERIENCE:
-      console.log('row', row);
       return isExperienceValid(value, row.age);
+    case YEARLY_INCOME:
+      return isYearlyIncomeValid(value);
     default:
       return true;
   }
