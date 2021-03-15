2d882f3618dd320c67ff9782e7626f1b30da3565
Nazar <wolfter12@gmail.com>
2021-03-15 23:36:56 +0200

    Added age validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,4 @@
-import { EMAIL, PHONE } from '../configs/header-accessors';
+import { AGE, EMAIL, PHONE } from '../configs/header-accessors';
 import { validate as emailValidator } from 'email-validator';
 
 export const isFullNameValid = (value) => {
@@ -26,12 +26,28 @@ export const isEmailValid = (value) => {
   return emailValidator(value);
 };
 
+// TODO: Ask the client about max age limit
+export const isAgeValid = (value) => {
+  // const checkAge = (age) => Number.isInteger(age) && age >= 21 && age <= 150;
+  const checkAge = (age) => Number.isInteger(age) && age >= 21;
+  switch (typeof value) {
+    case 'number':
+      return checkAge(value);
+    case 'string':
+      return checkAge(Number(value));
+    default:
+      return false;
+  }
+};
+
 const validator = (value, header) => {
   switch (header) {
     case PHONE:
       return isPhoneValid(value);
     case EMAIL:
       return isEmailValid(value);
+    case AGE:
+      return isAgeValid(value);
     default:
       return true;
   }
