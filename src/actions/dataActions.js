3e018409119d330b49d130983ddc6176075ab036
Nazar <wolfter12@gmail.com>
2021-03-16 01:51:54 +0200

    Converted hasChildren value to string

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,12 +1,18 @@
 import { PARSE_FILE } from './types';
 import csv from '../parser/csv';
 import getDuplication from '../utils/mark-duplication';
+import { HAS_CHILDREN } from '../configs/header-accessors';
 
 export const parseFile = (file) => (dispatch) => {
   if (file instanceof Blob) {
     csv(file)
       .then(({ data }) => data.map((obj, idx) => ({ id: idx + 1, ...obj })))
       .then((data) => getDuplication(data))
+      .then((data) =>
+        data.map((obj) => {
+          return { ...obj, [HAS_CHILDREN]: String(obj[HAS_CHILDREN]) };
+        })
+      )
       .then((data) => {
         console.log(data);
         dispatch({
