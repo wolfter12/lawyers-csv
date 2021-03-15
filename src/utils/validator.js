c6846902ad2aa44c5082cfaadf8a7eb64dbff377
Nazar <wolfter12@gmail.com>
2021-03-15 22:51:53 +0200

    Added full name validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,3 +1,10 @@
+export const isFullNameValid = (value) => {
+  if (typeof value !== 'string' || value.length === 0) {
+    return false;
+  }
+  return true;
+};
+
 const validator = (value, header) => {
   switch (header) {
     default:
