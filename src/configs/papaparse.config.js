e6499005d21b3496b8686613f67e498df44f35e8
Nazar <wolfter12@gmail.com>
2021-03-15 14:28:20 +0200

    Headers transform to camelCase

--- src/configs/papaparse.config.js
+++ src/configs/papaparse.config.js
@@ -1,7 +1,10 @@
+import camelCase from 'camelcase';
+
 const options = {
   header: true,
   worker: false, // when true there is a problem with next parsing
   skipEmptyLines: true,
+  transformHeader: camelCase,
 };
 
 export default options;
