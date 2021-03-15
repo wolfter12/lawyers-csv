a6386522d1b336cd6502d2040d0684655d34eb07
Nazar <wolfter12@gmail.com>
2021-03-15 13:16:24 +0200

    Deleted comment

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -5,7 +5,6 @@ const initialState = [];
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
     case PARSE_FILE:
-      console.log('action.payload', action.payload);
       return [...state, ...action.payload];
     default:
       return state;
