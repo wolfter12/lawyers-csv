5819bc17eb581a928c53327d53f533cccc0d8e00
Nazar <wolfter12@gmail.com>
2021-03-15 13:00:54 +0200

    Added input file, parsing button, show button

--- src/components/UploadFile/UploadFile.js
+++ src/components/UploadFile/UploadFile.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { parseFile } from '../../actions/dataActions';
 
 function UploadFile() {
+  const [file, setFile] = useState({});
+  const data = useSelector((state) => state.data);
+
+  const dispatch = useDispatch();
+
+  const changeHandler = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const parseSelectedFile = () => {
+    dispatch(parseFile(file));
+  };
+
+  const showData = () => {
+    console.log('Parsed data:', data);
+  };
+
   return (
     <div>
-      <h3>Upload File</h3>
+      <input type="file" name="file" onChange={changeHandler} />
+      <button onClick={parseSelectedFile}>Parse file</button>
+      <button onClick={showData}>Show Data</button>
     </div>
   );
 }
