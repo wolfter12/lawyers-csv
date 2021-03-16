2cb98bb974fd6f483c27bd12076794a5e7a0ad09
Nazar <wolfter12@gmail.com>
2021-03-16 16:31:32 +0200

    Added phone format regexs

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -3,3 +3,4 @@ export const DATE_FORMATS = ['YYYY-MM-DD', 'MM/DD/YYYY'];
 export const WARNING_MESSAGE = 'File format is not correct';
 export const WARNING_ALERT_VARIANT = 'danger';
 export const INVALID_CELL = 'table-danger';
+export const PHONE_FORMATS = [/^\+1\d{10}$/, /^1\d{10}$/, /^\d{10}$/];
