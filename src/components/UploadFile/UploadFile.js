e6b0b94f2be5309cf8acf9d92f05af075ed48397
Nazar <wolfter12@gmail.com>
2021-03-16 22:49:45 +0200

    Refactored

--- src/components/UploadFile/UploadFile.js
+++ src/components/UploadFile/UploadFile.js
@@ -1,31 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import ReactFileReader from 'react-file-reader';
 import { parseFile } from '../../actions/dataActions';
-import { Button, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
-const uploadOptions = {
-  fileTypes: ['.csv'],
-  multiplyFiles: false,
-};
-
 function UploadFile() {
-  const [file, setFile] = useState({});
-
   const dispatch = useDispatch();
-
-  const parseSelectedFile = () => {
-    dispatch(parseFile(file));
-    setFile({});
-  };
 
   const handleFiles = (files) => {
     const [file] = files;
     const { name } = file;
     console.log(file);
     if (name.split('.').pop() === 'csv') {
-      setFile(file);
+      dispatch(parseFile(file));
     }
   };
 
@@ -38,7 +26,6 @@ function UploadFile() {
       >
         <Button>Import users</Button>
       </ReactFileReader>
-      <Button onClick={parseSelectedFile}>Parse file</Button>
     </Container>
   );
 }
