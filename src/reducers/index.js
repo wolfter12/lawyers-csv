de547941a2eae9d659f63e5350a2b713439355ea
Nazar <wolfter12@gmail.com>
2021-03-16 23:13:20 +0200

    Deleted fileReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import dataReducer from './dataReducer';
 import validationReducer from './validationReducer';
-import fileReducer from './fileReducer';
 
 const rootReducer = combineReducers({
   data: dataReducer,
   valid: validationReducer,
-  file: fileReducer,
 });
 
 export default rootReducer;
