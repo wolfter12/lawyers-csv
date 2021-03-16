e3ce18fe81881c8be965193cbe3ad96953a97d8b
Nazar <wolfter12@gmail.com>
2021-03-16 17:13:01 +0200

    Added a comment

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTable } from 'react-table';
+import { INVALID_CELL } from '../../configs/constants';
 import columns from '../../configs/columns';
 import validator from '../../utils/validator';
 import BTable from 'react-bootstrap/Table';
-import { INVALID_CELL } from '../../configs/constants';
 
 function Table() {
   // TODO: useMemo to not recalculate on every single render
@@ -41,6 +41,7 @@ function Table() {
                 let isValid = validator(value, header, cell.row.original);
 
                 // TODO: find more safer way to add class
+                // Add class if data is invalid
                 const cellProps = { ...cell.getCellProps() };
                 if (!isValid) {
                   if (cellProps.hasOwnProperty('className')) {
