4c1807a4f810b3b688c53cfaaebeed89602192e8
Nazar <wolfter12@gmail.com>
2021-03-16 15:45:33 +0200

    Added validationReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import dataReducer from './dataReducer';
+import validationReducer from './validationReducer';
 
 const rootReducer = combineReducers({
   data: dataReducer,
+  valid: validationReducer,
 });
 
 export default rootReducer;
