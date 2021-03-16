5eacf6e1d4454eda876b2407e09e705ba2b845ea
Nazar <wolfter12@gmail.com>
2021-03-16 22:10:07 +0200

    Added ReactFileReader

--- src/components/UploadFile/UploadFile.js
+++ src/components/UploadFile/UploadFile.js
@@ -1,35 +1,43 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import ReactFileReader from 'react-file-reader';
 import { parseFile } from '../../actions/dataActions';
 import { Button, Form } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+
+const uploadOptions = {
+  fileTypes: ['.csv'],
+  multiplyFiles: false,
+};
 
 function UploadFile() {
   const [file, setFile] = useState({});
 
   const dispatch = useDispatch();
 
-  const changeHandler = (event) => {
-    const selectedFile = event.target.files[0];
-    if (selectedFile !== undefined) {
-      setFile(selectedFile);
-    }
-  };
-
   const parseSelectedFile = () => {
     dispatch(parseFile(file));
     setFile({});
   };
 
+  const handleFiles = (files) => {
+    const [file] = files;
+    const { name } = file;
+    console.log(file);
+    if (name.split('.').pop() === 'csv') {
+      setFile(file);
+    }
+  };
+
   return (
-    <Container fluid className="flex-column mt-4 mb-4">
-      <Form.Group>
-        <Form.File
-          label="Example file input"
-          accept=".csv"
-          onChange={changeHandler}
-        />
-      </Form.Group>
+    <Container fluid className="text-right mt-4 mb-4">
+      <ReactFileReader
+        fileTypes={['.csv']}
+        multiplyFiles={false}
+        handleFiles={handleFiles}
+      >
+        <Button>Import users</Button>
+      </ReactFileReader>
       <Button onClick={parseSelectedFile}>Parse file</Button>
     </Container>
   );
