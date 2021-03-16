d462328dada7166f264871c60a61a70fc657017f
Nazar <wolfter12@gmail.com>
2021-03-16 15:47:35 +0200

    Added Warning component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import UploadFile from '../UploadFile';
-import Table from '../Table';
-import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Table from '../Table';
+import Warning from '../Warning';
+import Container from 'react-bootstrap/Container';
 
 function App() {
+  const valid = useSelector((state) => state.valid);
+  console.log('app is valid', valid);
   return (
     <Container fluid>
       <UploadFile />
-      <Table />
+      {valid ? <Table /> : <Warning />}
+      <UploadFile />
     </Container>
   );
 }
