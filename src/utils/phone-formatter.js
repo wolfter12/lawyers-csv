bcc7d64a64009f5cdb916d9d14a3c7b8cdec0c23
Nazar <wolfter12@gmail.com>
2021-03-16 16:51:57 +0200

    Changed if condition to function

--- src/utils/phone-formatter.js
+++ src/utils/phone-formatter.js
@@ -1,12 +1,9 @@
 import phone from 'phone';
+import { PHONE_FORMATS } from '../configs/constants';
 
 // TODO: ask client about conditions
-// TODO: move regex to constants
 const phoneFormatter = (phoneNumber) => {
-  const isValid =
-    /^\+1\d{10}$/.test(phoneNumber) ||
-    /^1\d{10}$/.test(phoneNumber) ||
-    /^\d{10}$/.test(phoneNumber);
+  const isValid = PHONE_FORMATS.some((reg) => reg.test(phoneNumber));
   if (isValid) {
     return phone(phoneNumber)[0];
   }
