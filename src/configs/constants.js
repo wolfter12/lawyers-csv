39288b7d8e5327b666ca39aa0a5cf939ef306f40
Nazar <wolfter12@gmail.com>
2021-03-16 15:46:09 +0200

    Added new constants

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,2 +1,5 @@
 export const MAX_YEARLY_INCOME = 1000000;
 export const DATE_FORMATS = ['YYYY-MM-DD', 'MM/DD/YYYY'];
+export const WARNING_MESSAGE = 'File format is not correct';
+export const WARNING_ALERT_VARIANT = 'danger';
+export const INVALID_CELL = 'table-danger';
