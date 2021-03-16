4b7d6015e9a2f7b6886138f903803c00a08fe19d
Nazar <wolfter12@gmail.com>
2021-03-16 22:39:51 +0200

    Refactored license states foramtting

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,17 +1,12 @@
 import { PARSE_FILE, VALID_STRUCTURE } from './types';
-import {
-  HAS_CHILDREN,
-  LICENSE_STATES,
-  YEARLY_INCOME,
-} from '../configs/header-accessors';
 import { WARNING_MESSAGE } from '../configs/constants';
-import statesJSON from '../configs/states_titlecase.json';
 import csv from '../parser/csv';
 import addId from '../utils/add-id';
 import addDuplicationField from '../utils/add-duplication-field';
 import requiredFieldValidator from '../utils/csv-required-fields-validator';
 import convertYearlyIncome from '../utils/convert-yearly-income';
 import convertHasChildren from '../utils/convert-has-children';
+import convertLicenseStates from '../utils/convert-license-states';
 
 // TODO: convert all boolean to string
 // TODO: ask the client about a range of input formats
@@ -30,27 +25,7 @@ export const parseFile = (file) => (dispatch) => {
       .then(addDuplicationField)
       .then(convertYearlyIncome)
       .then(convertHasChildren)
-      .then((data) => {
-        return data.map((obj) => {
-          const currentDataStates = obj[LICENSE_STATES].split(/\s*\|\s*/);
-          const currentDataStatesAbbreviationArr = currentDataStates.map(
-            (str) => {
-              const state = statesJSON.find(({ name, abbreviation }) => {
-                return (
-                  name.toLowerCase() === str.toLowerCase() ||
-                  abbreviation.toLowerCase() === str.toLowerCase()
-                );
-              });
-              if (state !== undefined) {
-                return state.abbreviation;
-              }
-              return str; // If there is no such state in list return original value
-            }
-          );
-          const states = currentDataStatesAbbreviationArr.join(' | ');
-          return { ...obj, [LICENSE_STATES]: states };
-        });
-      })
+      .then(convertLicenseStates)
       .then((data) => {
         console.log(data);
         dispatch({
