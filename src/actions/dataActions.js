969ad146cf80e2bed5b217730b48a70dc393e88a
Nazar <wolfter12@gmail.com>
2021-03-16 13:26:13 +0200

    Converted states name to abbreviation

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,9 +1,11 @@
 import { PARSE_FILE } from './types';
 import csv from '../parser/csv';
 import getDuplication from '../utils/mark-duplication';
-import { HAS_CHILDREN } from '../configs/header-accessors';
+import { HAS_CHILDREN, LICENSE_STATES } from '../configs/header-accessors';
+import statesJSON from '../configs/states_titlecase.json';
 
 // TODO: convert all boolean to string
+// TODO: ask the client about a range of input formats
 export const parseFile = (file) => (dispatch) => {
   if (file instanceof Blob) {
     csv(file)
@@ -16,6 +18,27 @@ export const parseFile = (file) => (dispatch) => {
           return { ...obj, [HAS_CHILDREN]: hasChildren };
         })
       )
+      .then((data) => {
+        return data.map((obj) => {
+          const currentDataStates = obj[LICENSE_STATES].split(/\s*\|\s*/);
+          const currentDataStatesAbbreviationArr = currentDataStates.map(
+            (str) => {
+              const state = statesJSON.find(({ name, abbreviation }) => {
+                return (
+                  name.toLowerCase() === str.toLowerCase() ||
+                  abbreviation.toLowerCase() === str.toLowerCase()
+                );
+              });
+              if (state !== undefined) {
+                return state.abbreviation;
+              }
+              return str; // If there is no such state in list return original value
+            }
+          );
+          const states = currentDataStatesAbbreviationArr.join(' | ');
+          return { ...obj, [LICENSE_STATES]: states };
+        });
+      })
       .then((data) => {
         console.log(data);
         dispatch({
