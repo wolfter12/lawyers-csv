8a7ad23caecafe0acead7994b915a7793b7fc4fe
Nazar <wolfter12@gmail.com>
2021-03-16 16:43:45 +0200

    Added MIN_YEARLY_INCOME

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,3 +1,4 @@
+export const MIN_YEARLY_INCOME = 0;
 export const MAX_YEARLY_INCOME = 1000000;
 export const DATE_FORMATS = ['YYYY-MM-DD', 'MM/DD/YYYY'];
 export const WARNING_MESSAGE = 'File format is not correct';
