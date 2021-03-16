80f645e7dccf5ffc114d7324d5ef21ef8941f4d1
Nazar <wolfter12@gmail.com>
2021-03-16 13:13:37 +0200

    Changed has children validation conditions

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -112,8 +112,7 @@ export const isHasChildrenValid = (value) => {
   if (typeof value !== 'string') {
     return false;
   }
-  // if it is an empty string then field is valid
-  return value === 'true' || value === 'false' || value.length === 0;
+  return value === 'true' || value === 'false';
 };
 
 export const isLicenseNumberValid = (value) => {
