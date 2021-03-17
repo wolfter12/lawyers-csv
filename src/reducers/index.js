52553f1583b647badef04b3fb12ab825e9445e8e
Nazar <wolfter12@gmail.com>
2021-03-17 18:55:17 +0200

    Updated with loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import dataReducer from './dataReducer';
 import validationReducer from './validationReducer';
+import loadingReducer from './loadingReducer';
 
 const rootReducer = combineReducers({
   data: dataReducer,
   valid: validationReducer,
+  isLoading: loadingReducer,
 });
 
 export default rootReducer;
