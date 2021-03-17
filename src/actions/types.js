bc977f4a8b14da57add091b86c4f9e3c7592fec0
Nazar <wolfter12@gmail.com>
2021-03-17 18:54:26 +0200

    Added new action

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,4 @@ export const PARSE_FILE = 'PARSE_FILE';
 export const VALID_STRUCTURE = 'VALID_STRUCTURE';
 export const ADD_FILE_NAME = 'ADD_FILE_NAME';
 export const DELETE_DATA = 'DELETE_DATA';
+export const LOADING = 'LOADING';
