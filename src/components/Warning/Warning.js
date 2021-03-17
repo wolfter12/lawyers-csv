05161b71a0a4cce5b7f1ac49de9f413c3c573650
Nazar <wolfter12@gmail.com>
2021-03-17 02:11:34 +0200

    Added Container

--- src/components/Warning/Warning.js
+++ src/components/Warning/Warning.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
 import {
   WARNING_MESSAGE,
   WARNING_ALERT_VARIANT,
@@ -7,11 +8,13 @@ import {
 
 function Warning() {
   return (
-    <Alert variant={WARNING_ALERT_VARIANT} className="mt-4 mb-4">
-      <Alert.Heading className="text-center mt-4 mb-4">
-        {WARNING_MESSAGE || 'Oops, something went wrong'}
-      </Alert.Heading>
-    </Alert>
+    <Container fluid="md">
+      <Alert variant={WARNING_ALERT_VARIANT} className="mt-4 mb-4">
+        <Alert.Heading className="text-center mt-4 mb-4">
+          {WARNING_MESSAGE || 'Oops, something went wrong'}
+        </Alert.Heading>
+      </Alert>
+    </Container>
   );
 }
 
